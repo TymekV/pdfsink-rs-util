6f0c11b9fd128f9e814faf8e36c30340152cff98
Tymek <vonautymek@gmail.com>
2026-04-11 00:07:02 +0200

    style: removed unused comments

--- pdfsink-rs-util/src/lib.rs
+++ pdfsink-rs-util/src/lib.rs
@@ -2,32 +2,3 @@ pub use pdfsink_rs_util_macros::*;
 pub use table::*;
 
 mod table;
-
-// #[derive(FromPdfTable)]
-// struct Test {
-//     a: String,
-//     b: f64,
-// }
-
-// impl FromPdfTable for Test {
-//     fn try_parse_strict(table: &table::Table) -> Result<Vec<Self>, table::FromTableError> {
-//         let rows = table
-//             .iter()
-//             .map(|row| -> Result<Self, FromTableError> {
-//                 let a: String = row
-//                     .get(0)
-//                     .ok_or(FromTableError::ColumnNotFound { column: "a" })?
-//                     .clone()
-//                     .ok_or(FromTableError::MissingValue { column: "a" })?;
-
-//                 Ok(Self { a, b: 0.0 })
-//             })
-//             .collect::<Result<Vec<_>, _>>()?;
-
-//         Ok(rows)
-//     }
-
-//     fn try_parse(table: &table::Table) -> Vec<Self> {
-//         todo!()
-//     }
-// }
